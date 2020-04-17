3e97b703418888034ced34c6f9476f7c81d04f81
serch3128 <62315538+serch3128@users.noreply.github.com>
2020-04-16 22:07:35 -0500

    upload 2

--- pypoll/Analysis/main.py
+++ pypoll/Analysis/main.py
@@ -1,8 +1,11 @@
 import os
 import csv
-csvpath = os.path.join('..', 'Resources', '02-Homework_03-Python_Instructions_PyPoll_Resources_election_data.csv')
+csvpath = os.path.join('..', 'Resources', 'election_data.csv')
 votes=[]
 candidates=[]
+votesforeach=[0,0,0,0,0]
+votesforeachp=[]
+uniquecandidates=[]
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)      
@@ -11,12 +14,48 @@ with open(csvpath) as csvfile:
 
     for row in csvreader:
         votes.append(row[0])
+        candidates.append(row[1])
 
-voteCounter=len(votes)
-print(voteCounter)
+    voteCounter=len(votes)
 
-    for row in range(1,votecounter):
-        candidates.append(csvfile[row])
+    for row in candidates:
+        if row not in uniquecandidates:
+            uniquecandidates.append(row)
+        
+    for x in range(len(uniquecandidates)):
+        for row in candidates: 
+            if  row == uniquecandidates[x]:
+                votesforeach[x]=votesforeach[x]+1
+
+    for row in range(len(votesforeach)):
+        a=votesforeach[row]/voteCounter
+        votesforeachp.append(a)
+
+print(votesforeachp)
+
+
+print("Election Results")
+print("-----------------------------")
+print(f"Total Votes {voteCounter} ")
+print("-----------------------------")
+
+
+
+print(votesforeach)
+
+
+        
+
+print(uniquecandidates) 
+        
+
     
-    print(candidates)
+
+
+
+
+print(len(candidates))
+
+
+
         
